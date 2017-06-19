6a28ba3f71b9894407f087696122ad9712793cdf
Darek Deoniziak <darekdeoniziak@gmail.com>
2017-06-19 20:05:19 +0200

    added test for getting taxonomies by type

--- wordpressrest/src/test/groovy/gq/coderetort/wpclient/rest/multiple/TaxonomiesRequests.groovy
+++ wordpressrest/src/test/groovy/gq/coderetort/wpclient/rest/multiple/TaxonomiesRequests.groovy
@@ -1,8 +1,31 @@
 package gq.coderetort.wpclient.rest.multiple
 
-import gq.coderetort.wpclient.rest.base.CommonRequests;
+import gq.coderetort.wpclient.models.Taxonomy
+import gq.coderetort.wpclient.rest.base.CommonRequests
+import gq.coderetort.wpclient.rest.queries.Query
 
 class TaxonomiesRequests extends CommonRequests {
 
     Closure get = { restClient.getTaxonomies(it) }
+
+    def "get taxonomies by type"() {
+        given: "Type to check and query with specified request params"
+        String type = "post"
+        Query query = new Query.QueryBuilder()
+            .type(type)
+            .build()
+
+        when: "Taxonomies are downloaded from rest with given query"
+        List<Taxonomy> taxonomies = get(query)
+
+        then: "List of taxonomies should not be empty"
+        taxonomies != null
+        !taxonomies.isEmpty()
+
+        and: "each taxonomy should be of requested type"
+        taxonomies.each { taxonomy ->
+            assert taxonomy.types != null
+            assert taxonomy.types.contains(type)
+        }
+    }
 }
