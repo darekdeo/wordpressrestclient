5a0586d1b74cd78d9cc2b268a151bc55fe75411d
Darek Deoniziak <darekdeoniziak@gmail.com>
2017-06-20 10:09:18 +0200

    updated Query javadoc and fields

--- wordpressrest/src/main/groovy/gq/coderetort/wpclient/rest/queries/Query.groovy
+++ wordpressrest/src/main/groovy/gq/coderetort/wpclient/rest/queries/Query.groovy
@@ -8,66 +8,206 @@ import java.lang.reflect.Field;
 @Builder
 class Query {
 
+    /**
+     * Scope under which the request is made; determines fields present in response.
+     * <p>Default: view</p>
+     * <p>One of: view, embed, edit</p>
+     */
     String context
+
+    /**
+     * Current page of the collection.
+     * <p>Default: 1</p>
+     */
     Integer page
-    @JsonProperty("per_page")
-    Integer perPage
+
+    /**
+     * Maximum number of items to be returned in result set.
+     * <p>Default: 10</p>
+     */
+    @JsonProperty("per_page") Integer perPage
+
+    /**
+     * Limit results to those matching a string.
+     */
     String search
+
+    /**
+     * Ensure result set excludes specific IDs.
+     */
     List<Integer> exclude
+
+    /**
+     * Limit result set to specific IDs.
+     */
     List<Integer> include
+
+    /**
+     * Order sort attribute ascending or descending.
+     * <p>Default: desc</p>
+     * <p>One of: asc, desc</p>
+     */
     String order
-    @JsonProperty("orderby")
-    String orderBy
-    @JsonProperty("hide_empty")
-    Boolean hideEmpty
-//    Integer post
-//    String slug
+
+    /**
+     * Sort collection by object attribute.
+     * <p><b>Default</b><br>
+     * Posts: date<br>
+     * Categories: name<br>
+     * Tags: name<br>
+     * Pages: date<br>
+     * Comments: date_gmt<br>
+     * Media: date</p>
+     * <p><b>One of</b><br>
+     * Posts: date, relevance, id, include, title, slug<br>
+     * Categories: id, include, name, slug, term_group, description, count<br>
+     * Tags: id, include, name, slug, term_group, description, count<br>
+     * Pages: date, relevance, id, include, title, slug, menu_order<br>
+     * Comments: date, date_gmt, id, include, post, parent, type<br>
+     * Media: date, relevance, id, include, title, slug<br></p>
+     */
+    @JsonProperty("orderby") String orderBy
+
+    /**
+     * Whether to hide terms not assigned to any resources.
+     */
+    @JsonProperty("hide_empty") Boolean hideEmpty
+
+    /**
+     * The password for the resource if it is password protected.
+     */
     String password
+
+    /**
+     * Limit response to resources published after a given ISO8601 compliant date.
+     */
     String after
+
+    /**
+     * Limit result set to resources assigned to specific authors.
+     */
     List<Integer> author
-    @JsonProperty("author_exclude")
-    List<Integer> authorExclude
+
+    /**
+     * Ensure result set excludes resources assigned to specific authors.
+     */
+    @JsonProperty("author_exclude") List<Integer> authorExclude
+
+    /**
+     * Limit response to resources published before a given ISO8601 compliant date.
+     */
     String before
+
+    /**
+     * Offset the result set by a specific number of items.
+     */
     Integer offset
+
+    /**
+     * Limit result set to resources assigned a specific status.
+     * <p><b>Default</b><br>
+     * Posts: publish<br>
+     * Pages: publish<br>
+     * Comments: approve</p>
+     * <p><b>One of</b><br>
+     * Pages: publish, future, draft, pending, private, trash, auto-draft, inherit, any</p>
+     */
     String status
-    @JsonProperty("status")
-    List<String> statusList
+
+    /**
+     * Limit result set to resources assigned one or more statuses.
+     */
+    @JsonProperty("status") List<String> statusList
+
+    /**
+     * Limit result set to resources of specific parent ids.
+     */
     Integer parent
-    @JsonProperty("parent_exclude")
-    Integer parentExclude
-    @JsonProperty("parent")
-    List<Integer> parentList
-    @JsonProperty("parent_exclude")
-    List<Integer> parentExcludeList
-    @JsonProperty("author_email")
-    String authorEmail
+
+    /**
+     * Limit result set to resources assigned to a specific parent ids.
+     */
+    @JsonProperty("parent") List<Integer> parentList
+
+    /**
+     * Ensure result set excludes specific parent ids.
+     */
+    @JsonProperty("parent_exclude") List<Integer> parentExcludeList
+
+    /**
+     * Limit result set to that from a specific author email. Requires authorization.
+     */
+    @JsonProperty("author_email") String authorEmail
+
+    /**
+     * Limit result set to that of a particular comment karma. Requires authorization.
+     */
     Integer karma
+
+    /**
+     * Limit result set to terms assigned to a specific post.
+     */
     Integer post
-    @JsonProperty("post")
-    List<Integer> postList
+
+    /**
+     * Limit result set to resources assigned to specific post ids.
+     */
+    @JsonProperty("post") List<Integer> postList
+
+    /**
+     * Limit result set to resources assigned a specific type.
+     * <p>Default (Comments): comment</p>
+     */
     String type
-    @JsonProperty("menu_order")
-    Integer menuOrder;
+
+    /**
+     * Limit result set to resources with a specific menu_order value.
+     */
+    @JsonProperty("menu_order") Integer menuOrder
+
+    /**
+     * Limit result set to posts with one or more specific slugs.
+     */
     List<String> slug;
+
+    /**
+     * Use WP Query arguments to modify the response; private query vars require appropriate authorization.
+     */
     List<String> filter; // todo how to do
+
+    /**
+     * Limit result set to all items that have the specified term assigned in the categories taxonomy.
+     */
     List<Integer> categories
-    @JsonProperty("categories_exclude")
-    List<Integer> categoriesExclude
+
+    /**
+     * Limit result set to all items except those that have the specified term assigned in the categories taxonomy.
+     */
+    @JsonProperty("categories_exclude") List<Integer> categoriesExclude
+
+    /**
+     * Limit result set to all items that have the specified term assigned in the tags taxonomy.
+     */
     List<Integer> tags
-    @JsonProperty("tags_exclude")
-    List<Integer> tagsExclude
+
+    /**
+     * Limit result set to all items except those that have the specified term assigned in the tags taxonomy.
+     */
+    @JsonProperty("tags_exclude") List<Integer> tagsExclude
+
+    /**
+     * Limit result set to items that are sticky.
+     */
     Boolean sticky
 
     /**
      * Limit result set to attachments of a particular media type.
      * <p>One of: image, video, audio, application</p>
-     * <p><b>Applicable for:</b> Media</p>
      */
     @JsonProperty("media_type") String mediaType
 
     /**
      * Limit result set to attachments of a particular MIME type.
-     * <p><b>Applicable for:</b> Media</p>
      */
     @JsonProperty("mime_type") String mimeType
 
