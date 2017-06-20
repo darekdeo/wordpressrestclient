df0595741bc6aa3ae8cd5acda790ce5ac5a2bf01
Darek Deoniziak <darekdeoniziak@gmail.com>
2017-06-20 13:20:38 +0200

    removed unneded JsonAutodetect annotation

--- wordpressrest/src/main/groovy/gq/coderetort/wpclient/models/base/Entry.groovy
+++ wordpressrest/src/main/groovy/gq/coderetort/wpclient/models/base/Entry.groovy
@@ -1,13 +1,8 @@
 package gq.coderetort.wpclient.models.base
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonProperty
 import gq.coderetort.wpclient.models.Object
 
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE
-
-@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
 abstract class Entry extends Creation {
 
     /**
